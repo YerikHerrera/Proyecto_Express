559daff10ca2678f7c5b7d57cb0316646acb3ff9
ElYendrik <elyendriklamar@gmail.com>
2026-07-31 10:17:18 -0500

    feat: configurar variables de entorno, script de desarrollo y documentar comandos

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
-import express from 'express';
+import express from 'express'
+import {configDotenv} from "dotenv"
+
+configDotenv()
+
 const app = express();
-const puerto = process.env.PUERTO || 3030
+const puerto = process.env.PUERTO || 3000
 
 app.get("/", function(req, res){
-  res.send(`Hola, estoy aprendiendo express, ficha 3407181 programa ADSO`);
+  res.send(`Hola, estoy aprendiendo express, ficha 3407181 programa ADSO, 31 de julio`);
 });
 
 app.listen(puerto, () => {
