915a2028d4cd9be462e720faf533689ad844e378
ElYendrik <elyendriklamar@gmail.com>
2026-08-21 11:25:59 -0500

    Creación del endpoint listar aprendices junto al archivo datos.json

--- app.js
+++ app.js
@@ -2,10 +2,26 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
-
-app.get("/", (req, res) => {
+//configurar para la lectura del archivo
+const sistemaArchivo = require("fs")
+const ruta = require("path")
+const rutaArchivoJson = ruta.join(__dirname, "datos.json")
+//endpoint raiz
+app.get("/", function(req, res){
   res.send('API Rest - Aprendices');
-});
+})
+
+//endpoint para ver los datos del archivo
+app.get("/api/aprendices", (req, res)=>{
+  //datos vienen del archivo
+  sistemaArchivo.readFile(rutaArchivoJson, "utf-8", (error, datos)=>{
+    if (error){
+      return res.json({Error: "No se puede leer los datos."})
+    }
+    const listaAprendices = JSON.parse(datos)
+    res.json(listaAprendices)
+  })
+})
 
 app.listen(PORT, () => {
   console.log(`Servidor en corriendo en el puerto: ${PORT}`);
