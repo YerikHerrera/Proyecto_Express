0685bb51523360e7a61ee47237aff44e68f9d44c
ElYendrik <elyendriklamar@gmail.com>
2026-08-21 10:24:27 -0500

    Segundo commit inicio proyecto

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
+require('dotenv').config();
 const express = require('express');
 const app = express();
-const port = 3000;
+const PORT = process.env.PORT || 3000;
 
-app.get("/", (_, res) => {
-  res.send('Aprendiendo express, ficha 3407181');
+app.get("/", (req, res) => {
+  res.send('API Rest - Aprendices');
 });
 
-app.listen(port, () => {
-  console.log(`Servidor en funcionamiento en el puerto: ${port}`);
+app.listen(PORT, () => {
+  console.log(`Servidor en corriendo en el puerto: ${PORT}`);
 });
