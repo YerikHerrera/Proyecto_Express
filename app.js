c90ba0c1f444c968172588130333ce3cd79d8cdc
ElYendrik <elyendriklamar@gmail.com>
2026-08-28 08:53:07 -0500

    Actividad: Creación de endpoint de validación y manejo de datos no encontrados.

--- app.js
+++ app.js
@@ -49,6 +49,26 @@ app.post("/ruta2",(req, res)=>{
     res.status(201).json({Datos: todosDatos, nombre: name, cargo: lastname})
 })
 
+//ACTIVIDAD: Endpoint de validación y manejo de recursos no encontrados
+app.post("/login", (req, res)=>{
+  const usuario = req.body.usuario
+  const perfil = req.body.perfil
+  const contraseña = req.body.contraseña
+  //validar si falta algun dato
+  if (!usuario || !perfil || !contraseña){
+    return res.send("Faltan datos por llenar.")
+  }
+
+  //validar perfil y dar acceso o bienvenida
+  if (perfil === 'admin')
+    return res.send(`Bienvenido admininistrador: ${usuario}`)
+  if (perfil === 'user')
+    return res.send(`Bienvenido usuario: ${usuario}`)
+
+  //si no es ninguno, no tiene acceso
+  res.send("Perfil inexistente. Acceso denegado.")
+})
+
 app.listen(puerto, function(){
   console.log(`SERVIDOR: http://localhost:${puerto}`);
 });
